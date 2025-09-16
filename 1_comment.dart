c1feefe313b7f8c11fcad21f9452c36d2c1dcbee
Dzaka29 <aniszaka29@gmail.com>
2025-09-16 14:26:17 +0700

    add dart 1_comment

---
+++ 1_comment.dart
@@ -0,0 +1,19 @@
+//1.single line comment menggunakan  //
+// 2.multi line comment menggunakan /* ... */
+// 3.documentasi comment menggunakan /// atau  /** */
+/** 
+ * ini adalah contoh  penggunaan multi line comment
+ * yang lebih dari satu baris
+ * dan biasanya dingunakan untuk dokumentasi
+*/
+/**
+ * ini adalah contoh dokumentasi comment
+ * program nya yang pertama dijalankan adalah [main], kemudian function [multiply]akan mengembalikan nilai perkalian 5 * 5
+ */
+void main(List<String> args) {
+  print("Dart is fun");
+  print("5 * 5 = ${multiply()}")
+}
+int muliply(){
+  return 5 * 5;
+}
