21d47c840693f14849de7b511f5edca4e55af7e4
Dzaka29 <aniszaka29@gmail.com>
2025-09-18 14:20:56 +0700

    Belajar inpur

---
+++ 3_type_data.dart
@@ -0,0 +1,26 @@
+// Variable adalah suatu wadah untuk menyimpan data
+/**
+ * Tipe Data adalah suatu konsep yang digunakan untuk mengklasifikasikan jenis data yang dapat diproses oleh progam
+ */
+/**
+ * jenis jenis tipe data
+ * int => integer => bilanga bulat => 1, -2, 11 dll
+ * double => bilangan desimal => 3.14, -0.1, 2.5 dll
+ * num => gabungan dari integer dan double => 3.14, 2, -2,  0, 3.15
+ * bool => boolean true or false
+ * string = string = "kumpulan karakter di dalam petik"
+ * list => berupa daftar => [1, 2, 3, 4, 5,] [a, b, c]
+ * map => berupa key:value => {"nama_kota" : "jakarta", "ID" : 62}
+ * Dynamic => campuran /tipe apapun => [1, 'a', {"nama_kota" : "Jakarta"} ]
+ */
+
+void main(List<String> args) {
+  String nama = "ucup" ;
+  int age = 24
+  var tinggi_badan;
+  tinggi_badan = 159
+  tinggi_badan = "Seratus enam puluh"
+  
+  bool IsMaried = false;
+  double Berat_Badan = 67.7 ;
+}
--- contoh.dart
+++ contoh.dart
@@ -1,15 +1,6 @@
-import 'dart:developer';
 
 import '2_const_final.dart';
 
 void main(List<String> args) {
-   final tanggal = "19" ;
-   final bulan = "September" ;
-   var namaku = "Bintungsky" ;
-   
-  print("Hallo ${umuraku(time)}");
-}
 
-num umuraku(num time){
-  return phi * time * time ;
 }
