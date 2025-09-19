cdb10a53600b2c9ce0f47e2d64da129c35ed03ee
Dzaka29 <aniszaka29@gmail.com>
2025-09-19 10:31:37 +0700

    belajar_String

---
+++ 6_String.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+ 
+  // ada single quote dan double quote 
+  // '' ini single quote
+  //"" ini double quote
+
+  String singlequote = 'Apa kabar kamu ?';
+
+ print('"Apa kabar kamu", tanya umar');
+  String doublequote = "Alhamdullilah baik";
+ // kalau dibali print(""Apa kabar kamu", tanya umar")
+ print('$singlequote dan $doublequote');
+print("$singlequote dan $doublequote");
+ var kalkulasi = 'Angka';
+ print('$kalkulasi = ${1 + 1}');
+
+ //print $
+ //ini simbol \ adalah backsplash
+ // untuk menghindari karakter khusus
+ print('\$ ini adalah symbol dollar $kalkulasi');
+ print(r'$1000.000.00);
+ //\unicode
+ print('\u{1F600}');
+ print('\nuDnia');
+}
+ 
