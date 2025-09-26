4ac3347e924df14015ec8890e4aa6354fb1e1b61
Dzaka29 <aniszaka29@gmail.com>
2025-09-26 14:22:46 +0700

    belajar for loop

---
+++ 14_for_loop.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  /**
+   * fot loop digunakan untuk melakukan perulangan kode 
+   * yang dimana jika sudah sampai di hasil yang ditentukan
+   * maka program akan berhenti
+   * jika program yang dijalankan tidak berhenti 
+   * (kesalahan kode) maka akan mengalami infinity loop
+   * sampai crash biasanya
+   */
+  // i adalah variable
+  for (var index = 0; index <= 10; index++) {
+    print('Index ke$index');
+  }
+
+  //perulangan dalam perulangan
+  //atau nested loop
+  // perulangan bersarang
+
+  for (var i = 1; i < 6; ++) {
+    for (var j = 1; j < i count; ++)
+  }
+}
